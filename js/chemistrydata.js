7806c1527ce56f7dcd2176b622b02f2e1594bfd7
fatmaalsolami <95140005+fatmaalsolami@users.noreply.github.com>
2021-12-29 12:25:44 +0300

    Create chemistrydata.js

---
+++ js/chemistrydata.js
@@ -0,0 +1,406 @@
+const chemistry = [
+    {
+    year: "2021",
+    list: [
+      {
+        title: "#",
+        subtitle: "#",
+        italic: "#",
+        pp: "#",
+        url: "#",
+      },
+      
+      ]
+    },
+    {
+    year: "2020",
+    list: [
+      {
+        title: "Wessam Nadar, ",
+        subtitle: "“A novel and potential chemical sensor for effective monitoring of Fe(II) ion in corrosion systems of water samples. “",
+        italic: " Microchemical Journal, ",
+        pp: " (2020)",
+        url: "#",
+      },
+      {
+        title: "Wessam Nadar, ",
+        subtitle: "“Novel synthesis, structure characterization, DFT and investigation of the optical properties of cyanine dye/zinc oxide [4-CHMQI/ZnO] C nanocomposite thin film.”, ",
+        italic: Journal of Molecular Structure. ",
+        pp: " (2020).",
+        url: "#",
+      },
+      {
+        title: "أحمد سليمان عيسى,",
+        subtitle: "” Effects of kenaf bast fibre and silica fume content on bending strength and dimensional stability of cement bonded kenaf composite boards.”, ",
+        italic: "Journal of Engineering Science and Technology. ",
+        pp: " (2020).",
+        url: "#",
+      },
+      {
+        title: "فتح الرحمن حامد الحسن,",
+        subtitle: "” Optimization of the esterification process of crude jatropha oil (CJO) containing high levels of free fatty acids: a Malaysian case study”, ",
+        italic: "Biofuels. ",
+        pp: "(2020).",
+        url: "#",
+      },
+      {
+        title: "Jaafer M. Alkabli & Wessam Nadar, ",
+        subtitle: "“Upgrading Oryza sativa wastes into multifunctional antimicrobial and antibiofilm nominees; Ionic Metallo-Schiff base-supported cellulosic nanofibers” ",
+        italic: "European Polymer Journal ",
+        pp: " (2020).",
+        url: "#",
+      },
+      {
+        title: "Jaafer M. Alkabli, ",
+        subtitle: "” Upgrading Oryza sativa wastes into multifunctional antimicrobial and antibiofilm nominees; Ionic Metallo-Schiff base-supported cellulosic nanofibers” ",
+        italic: "European Polymer Journal ",
+        pp: " (2020).",
+        url: "#",
+      },
+      {
+        title: "Jaafer M. Alkabli & Wessam Nadar,",
+        subtitle: "” Azomethine-functionalized task-specific ionic liquid for diversion of toxic metal ions in the aqueous
+environment into pharmacological nominates”",
+        italic: " Journal of Molecular Liquids-, ",
+        pp: " (2020). ",
+        url: "#",
+      },
+      
+      ]
+    },
+    {
+      year: "2019",
+      list: [
+        {
+          title: "Wessam Nadar,",
+          subtitle: "” Investigation of novel nanomaterial for the removal of toxic substances from contaminated water”, ",
+          italic: "RSC Adv ",
+          pp: " (2019).",
+          url: "#",
+        },
+        {
+          title: "أحمد سليمان عيسى,",
+          subtitle: "” Ziziphus spina-christi: Analysis of Bioactivities and Chemical Composition. In: A.A. Mariod, Editors. Wild Fruits: Composition, Nutritional Value and Products”, ",
+          italic: "Springer Nature Switzerland AG. ",
+          pp: " (2019) ",
+          url: "#",
+        },
+        {
+          title: "فتح الرحمن حامد الحسن,",
+          subtitle: "” Characterization of thymoquinone/hydroxypropyl-β-cyclodextrin inclusion complex: Application to anti-allergy properties”, ",
+          italic: "European Journal of Pharmaceutical Sciences, ",
+          pp: " (2019)",
+          url: "#",
+        },
+        
+        ]
+    },
+    {
+      year: "2018",
+      list: [
+        {
+          title: "Ameen Bin Ali and Wessam Nadar, ",
+          subtitle: "“Feed and Bleed Bipolar Membrane Electrodialysis Process,” ",
+          italic: "Middle East Journal of Applied Sciences. ",
+          pp: " (2018)",
+          url: "#",
+        },
+        {
+          title: "فتح الرحمن حامد الحسن",
+          subtitle: "” Electrochemical Detection of ascorbic acid using Fe-Mn-SO42-/ZrO2 (FMSZ) Modified Glassy Carbon Electrode. ”",
+          italic: " Journal of Pure and Applied Sciences. ",
+          pp: " (2018).",
+          url: "#",
+        },
+        
+        ]
+      },
+      {
+      year: "2017",
+      list: [
+        {
+          title: "أحمد سليمان عيسى,",
+          subtitle: "” Physical Mechanical Characteristics of Cement-Bonded Kenaf Composite Boards with different Densities”,",
+          italic: " Journal of Engineering Science and Technology. ",
+          pp: " (2017) ",
+          url: "#",
+        },
+        {
+          title: "فتح الرحمن حامد الحسن, ",
+          subtitle: "“Solid-phase and precipitation synthesis of Ti-pyrophosphate for the catalytic oxydehydrogenation of n-butane”, ",
+          italic: "Comptes Rendus Chimie.",
+          pp: "(2017)",
+          url: "#",
+        },
+        
+        ]
+      },
+      {
+      year: "2016",
+      list: [
+        {
+          title: "Wessam Nadar, ",
+          subtitle: "“Synthesis and Characterization of a Proton Pump Inhibitor With Bivalent Metal ions”, ",
+          italic: "International Journal of Current Research in. ",
+          pp: " (2016).",
+          url: "#",
+        },
+        {
+          title: "Ameen Bin Ali and Wessam Nadar, ",
+          subtitle: "“Synthesis, characterization and DNA-Binding properties of the novel mononuclear Zn(II), Cd(II), and Mn(II) complexes with Pantoprazole”. ",
+          italic: "Mediterranean Journal of Chemistry. ",
+          pp: "(2016) ",
+          url: "#",
+        },
+        {
+          title: "Wessam Nadar, ",
+          subtitle: "“Pantoprazole Sodium Sesquihydrate Complexes: Synthesis, Characterization, Potentiometric Determination and DNA interaction”. ",
+          italic: "Oriental Journal of Chemistry. ",
+          pp: " (2016).",
+          url: "#",
+        },
+        {
+          title: "Wessam Nadar, ",
+          subtitle: "“Inhibitive Efficiency of Some Cellulosic materials as Corrosion Inhibitors for Mild Steel in petroleum Industry”.",
+          italic: " Journal of Scientific Research in Science ",
+          pp: " (2016)",
+          url: "#",
+        },
+        {
+          title: "فتح الرحمن حامد الحسن, ",
+          subtitle: "“Optimization of simultaneous production of waste cooking oil based-biodiesel using iron-manganese doped zirconia-supported molybdenum oxide nanoparticles catalyst” ",
+          italic: "Journal of Renewable and Sustainable Energy ",
+          pp: "(2016)",
+          url: "#",
+        },
+        {
+          title: "فتح الرحمن حامد الحسن, ",
+          subtitle: "”Ferric-manganese doped sulphated zirconia nanoparticles catalyst for single-step biodiesel production from waste cooking oil: Characterization and optimization‏”, ",
+          italic: "International Journal of Green Energy",
+          pp: "Procedia Engineering",
+          url: "#",
+        },
+        {
+          title: " فتح الرحمن حامد الحسن, ",
+          subtitle: "Isopropanolysis of Cottonseed Oil to Biodiesel via Potassium Hydroxide Catalyst‏",
+          italic: " Procedia Engineering.",
+          pp: " (2016).",
+          url: "#",
+        },
+        ]
+      },
+      {
+      year: "2015",
+      list: [
+        {
+          title: "Ashraf A. Sediek, ",
+          subtitle: "“Synthesis and Characterization of Some Novel Substituted Pyridones and Iminopyridines Derived from Pyrene Moiety”, ",
+          italic: "Russian Journal of General Chemistry ",
+          pp: " (2015).",
+          url: "#",
+        },
+        {
+          title: "Ameen Bin Ali, ",
+          subtitle: "“Ions transfer modeling through monopolar and bipolar membranes: Treatment of wastewater containing ammonium nitrate by Electrodialysis”. ",
+          italic: "Mediterranean Journal of Chemistry. ",
+          pp: " (2015)",
+          url: "#",
+        },
+        {
+          title: "Wessam Nadar, ",
+          subtitle: "“Determination of Radioactivity Levels of both Natural and Anthropogenic Radionuclides in Suez Canal”, ",
+          italic: "International Journal of Environmental Sciences, ",
+          pp: "(2015).",
+          url: "#",
+        },
+        {
+          title: "فتح الرحمن حامد الحسن, ",
+          subtitle: "“Cytotoxicity and physicochemical characterization of iron–manganese-doped sulfated zirconia nanoparticles‏”. ",
+          italic: "International journal of nanomedicine. ",
+          pp: " (2015)",
+          url: "#",
+        },
+        {
+          title: "فتح الرحمن حامد الحسن, ",
+          subtitle: "“Physicochemical properties, cytotoxicity, and antimicrobial activity of sulphated zirconia nanoparticles”, ",
+          italic: "International journal of nanomedicine, ",
+          pp: " (2015).",
+          url: "#",
+        },
+        {
+          title: "فتح الرحمن حامد الحسن, ",
+          subtitle: "“Synthesis of waste cooking oil based biodiesel via ferric-manganese promoted molybdenum oxide/zirconia nanoparticle solid acid catalyst: influence of ferric and manganese dopant”, ",
+          italic: "Journal of Oleo Science. ",
+          pp: "(2015) ",
+          url: "#",
+        },
+        {
+          title: "فتح الرحمن حامد الحسن ",
+          subtitle: "“Solution of Linear and Nonlinear Schrodinger Equations by Combine Elzaki Transform and Homotopy Perturbation Method”. ",
+          italic: "American Journal of Theoretical and Applied Statistics. ",
+          pp: "Vol. 4, No. 6, 2015, pp. 534-538, (2015). doi: 10.11648/j.ajtas. 20150406.24.",
+          url: "#",
+        },
+        {
+          title: "فتح الرحمن حامد الحسن,",
+          subtitle: " “Synthesis of ferric–manganese doped tungstated zirconia nanoparticles as heterogeneous solid super-acid catalyst for biodiesel production from waste cooking oil”,",
+          italic: "International journal of green energy. ",
+          pp: " (2015).",
+          url: "#",
+        },
+        {
+          title: "فتح الرحمن حامد الحسن, ",
+          subtitle: "“Biodiesel synthesis catalyzed by transition metal oxides: Ferric-manganese doped tungstated/molybdenum nanoparticle catalyst”, ",
+          italic: "Journal of oleo science. ",
+          pp: " (2015).",
+          url: "#",
+        },
+        {
+          title: "فتح الرحمن حامد الحسن, ",
+          subtitle: "“Synthesis of waste cooking oil-based biodiesel via effectual recyclable bi-functional Fe2O3MnOSO42−/ZrO2 nanoparticle solid catalyst”,",
+          italic: " Fuel, ",
+          pp: " (2015).",
+          url: "#",
+        },
+      
+        ]
+      },
+      {
+      year: "2014",
+      list: [
+        {
+          title: "Ashraf A. Sediek, ",
+          subtitle: "“One-pot three-component Kabachnik–Fields reaction: Synthesis of novel α-aminophosphonates containning thiazolylpyrazole moiety”, ",
+          italic: "Life Science Journal. ",
+          pp: " (2014)",
+          url: "#",
+        },
+        {
+          title: "Wessam Nadar, ",
+          subtitle: "corrosion inhibition and biological effect of some laboratory synthesis cationic surfactants in oil fields”,
+          italic: "Corrosion and Dye, ",
+          pp: " (2014).",
+          url: "#",
+        },
+        {
+          title: "Wessam Nadar, ",
+          subtitle: "synergistic effect of copper sulphate with some traditional biocides for killing the sulfate reducing bacteria in oil fields. ",
+          italic: "Middle East Journal of Applied Sciences, ",
+          pp: " (2014).",
+          url: "#",
+        },
+        {
+          title: "Wessam Nadar, ",
+          subtitle: "Preparation of some cationic surfactants and evaluation of their effect as corrosion inhibitors for tubing steel and antimicrobial agents for sulfate reducing bacteria ",
+          italic: "Middle East Journal of Applied Sciences, ",
+          pp: " (2014)",
+          url: "#",
+        },
+        {
+          title: "فتح الرحمن حامد الحسن, ",
+          subtitle: "“The effect of sulfate contents on the surface properties of iron–manganese doped sulfated zirconia catalysts‏”, ",
+          italic: "Powder technology. ",
+          pp: " (2014)",
+          url: "#",
+        },
+        {
+          title: "فتح الرحمن حامد الحسن, ",
+          subtitle: "“Hydrogen Production Via Ethanol Steam Reforming Over Ni/Al2O3 Catalysts: Effect of Ni Loading”, ",
+          italic: "J. Energy Resour. Technol. ",
+          pp: " (2014)",
+          url: "#",
+        },
+        
+        ]
+      },
+      {
+      year: "2013",
+      list: [
+        {
+          title: "Mohammad El-khateeb",
+          subtitle: "“Synthesis and characterization of platinum and palladium pyrrolidinedithiocarbamate complexes“",
+          italic: "Transition Met. Chem.",
+          pp: "(2013)",
+          url: "#",
+        },
+        {
+          title: "Mohammad El-khateeb",
+          subtitle: "“Synthesis and structural characterization of asymmetric mononuclear ruthenium (II) complexes derived from 2-(1,2,3-thiadiazol-4-yl)pyridine and azoimine ligands“",
+          italic: "Inorganica Chimica Acta",
+          pp: " (2013) ",
+          url: "#",
+        },
+        {
+          title: "Mohammad El-khateeb",
+          subtitle: "“Substitution reactions of [Fe-Fe]-hydrogenase model complexes containing diselenolate bridges“",
+          italic: "Polyhedron",
+          pp: " (2013)",
+          url: "#",
+        },
+        {
+          title: "Ashraf A. Sediek",
+          subtitle: "“Novel 2-pyrazolin-1-ylthiazoles as Potential Antimicrobial Agents“",
+          italic: "Letters in Drug Design and Discovery, ",
+          pp: " (2013)",
+          url: "#",
+        },
+
+        ]
+      },
+      {
+      year: "2012",
+      list: [
+        {
+          title: "Mohammad El-khateeb",
+          subtitle: "New Approach to [Fe-Fe]-hydrogenase Models Using Aromatic Thioketones ",
+          italic: "Eur. J. Inorg. Chem., 318.",
+          pp: "(2012)",
+          url: "#",
+        },
+        {
+          title: "Mohammad El-khateeb",
+          subtitle: "“Synthesis and reactivity of cyclopentadienyl iron complexes containing ferrocenyl selenolates”",
+          italic: "Transition Met Chem",
+          pp: "(2012) ",
+          url: "#",
+        },
+        {
+          title: "Mohammad El-khateeb",
+          subtitle: "“O-Alkylthiooxalato and dithiocarboxylato complexes of molybdenum and tungsten“",
+          italic: "Polyhedron",
+          pp: " (2012)",
+          url: "#",
+        },
+        {
+          title: "Mohammad El-khateeb",
+          subtitle: " “Heterocyclic thiocarboxylato complexes of iron: synthesis, characterization, electrochemistry, and reactions“",
+          italic: "Journal of Coordination Chemistry.",
+          pp: " (2012)",
+          url: "#",
+        },
+        {
+          title: "Mohammad El-khateeb",
+          subtitle: "“Synthesis and Reactivity of Mono-, Di-, and, Triiron Selenocarboxylate Complexes Z“",
+          italic: "Anorg. Allg. Chem.",
+          pp: " (2012)",
+          url: "#",
+        },
+        {
+          title: "Mohammad El-khateeb",
+          subtitle: "Comparison of S and Se dichalcogenolato [FeFe]-hydrogenase models with central S and Se atoms in the bridgehead",
+          italic: "chain. Tetrahedron ",
+          pp: "(2012)",
+          url: "#",
+        },
+		{
+          title: "Ashraf A. Sediek",
+          subtitle: "Design of new arylamino-2-ethane-1,1-diyl- and benzoxazole-2-methylenebisphosphonates vs cytotoxicity and chronic inflammation diseases. From hydrophobicity prediction to synthesis and biological evaluation",
+          italic: "European Journal of Medicinal Chemistry ",
+          pp: "(2012)",
+          url: "#",
+        },
+        ]
+      },
+    ]
+
+export default chemistry
