98f94a1ce65ea836097d8db1e073698dcafdbb90
fatmaalsolami <95140005+fatmaalsolami@users.noreply.github.com>
2022-01-03 12:55:12 +0300

    Create englishdata.js

---
+++ js/englishdata.js
@@ -0,0 +1,174 @@
+
+const English = [
+    {
+    year: "2021",
+    list: [
+      {
+        title: "Orabi, Hebatullah. ",
+        subtitle: "“Framing the Effects of Metaphor on Persuasion: A Critical Analysis of the Saudi Journalistic War against Coronavirus. ” ",
+        italic: "The first virtual forum of the College of Social Sciences at the University of Jeddah. ",
+        pp: " (2021).",
+        url: "#",
+      },
+      ]
+    },
+    {
+    year: "2020",
+    list: [
+      {
+        title: "Al-Momani, Abdullah, ",
+        subtitle: "“Measuring Vocabulary Knowledge of English as a Foreign Language among Undergraduate Jordanian Students. “",
+        italic: " International Journal of Linguistics, Literature and Translation (IJLLT), ",
+        pp: "2020.3.7.12, (2020).",
+        url: "#",
+      },
+      {
+        title: "Al-Momani, Abdullah, ",
+        subtitle: "“The Effectiveness of Internet Usage in the Period of (COVID 19) New Ear of Online Teaching. ”, ",
+        italic: "International Journal of Arts, Humanities and Social Studies (IJAHSS). ",
+        pp: "2582 – 3647, (2020).",
+        url: "#",
+      },
+      ]
+    },
+    {
+      year: "2019",
+      list: [
+        {
+          title: "Al-Momani, Abdullah,",
+          subtitle: "” IMPACT OF GREAT VOWEL SHIFT ON ARAB LEARNERS’USE OF ENGLISH SPELLING.”, ",
+          italic: "International Journal of English Language and Literature Studies, ",
+          pp: "8(2), 69-77, (2019).",
+          url: "#",
+        },
+        ]
+    },
+    {
+      year: "2018",
+      list: [
+        {
+          title: "Borni Lafi, ",
+          subtitle: "“Displacement, Separation, and Alienation: A Socio-Cultural Approach to Mistranslated Signs!  (Co-author Ziad Saad Mohamed Ali Alsulami). ” ",
+          italic: "International Journal of Humanities and Social Sciences Invention. (IJHSSI). ISSN (Online). ",
+          pp: "2319-7727, ISSN (Print): 2319-7714  || Volume 7 Issue 05 Ver. IV || May.2018 || PP.13-21. PDF.",
+          url: "https://www.ijhssi.org",
+        },
+        {
+          title: "Borni Lafi,",
+          subtitle: "”Erskine Caldwell, Zora Neale Hurston and Life’s Lower Elements in the South of the United States: Celebrating Region and Race! English Department’s Proceedings of Celebration. Research Lab: Innovation in Research Methodologies and Pedagogy in Humanities”",
+          italic: " Published by University of Kairouan. College of Arts and Humanities. 2018. Imprimerie Contact Sfax. ",
+          pp: "ISBN 978-9938-819-16-8. pp. 59-76, (2018).",
+          url: "#",
+        },
+        ]
+      },
+      {
+      year: "2017",
+      list: [
+        {
+          title: "Alzahrani, Mohammed,",
+          subtitle: "” The US Reception of Girls of Riyadh",
+          italic: " paper presented at  the Binghamton University Graduate Conference on Translation and Translation Studies, ",
+          pp: "April 21-23, (2017).",
+          url: "#",
+        },
+        ]
+      },
+      {
+      year: "2016",
+      list: [
+        {
+          title: "Borni Lafi, ",
+          subtitle: "“The Sons of Ham in the ‘American Promised Land’: The Biblical Tales and Afro-Americans from Slavery Time to the Civil Rights Movement. Research Lab: Innovation in Research Methodologies and Pedagogy in Humanities. ”, ",
+          italic: "published by University of Kairouan. Doctoral School. College of Arts and Humanities. Myths in Sacred Texts    (الأسطورة في النصوص المقدسة) . ",
+          pp: "Nouha Editions. 2016. Sfax. Tunisia. I.S.B.N: 978-9938-819-09-0. pp. 3-17, (2016).",
+          url: "#",
+        },
+        ]
+      },
+      {
+      year: "2015",
+      list: [
+        {
+          title: "Al-Momani, Abdullah, Supyan Hussin, and Afendi Hamat. ",
+          subtitle: "“An Investigation of Smartphone Reading Strategies Behaviours from the Views of Jordanian Students”, ",
+          italic: "Arab World English Journal 6.1 ",
+          pp: " (2015).",
+          url: "#",
+        },
+        {
+          title: "Borni Lafi, ",
+          subtitle: "“Tradition and Change in the 1930s/1940s American South”. ",
+          italic: "Lettres de Kairouan. ",
+          pp: "N. 11. December 2015. LATRACH EDITION. Tunis. I.S.S.N: 0330-6771. pp. 71-92. (2015). ",
+          url: "#",
+        },
+        {
+          title: "Borni Lafi, ",
+          subtitle: "“Modernity and its Problematics in the Arab World and in the South of the United States from the 1920s until the 1930s: Common Historical and Cultural Denominators! (Co-author Abdullatif Alsulami) in .اشكالية الحداثة في العالم العربي 2015.”, ",
+          italic: "published by University of Kairouan- Doctoral School, ",
+          pp: "Nouha Editions. 2015. Sfax. Tunisia. I.S.B.N: 978-9938-14-025-5. pp. 281-294, (2015).",
+          url: "#",
+        },
+        {
+          title: "Borni Lafi, ",
+          subtitle: "“The Harlem Renaissance and the Advent of “The New Negro”: The Debate. Al-Mukhatabat”. ",
+          italic: "#",
+          pp: "ISSN 1737-6432. Issue N* 15. July 2015. ARABESQUES Edition. pp. 133-150.",
+          url: "#",
+        },
+        ]
+      },
+      {
+      year: "2014",
+      list: [
+        {
+          title: "Al-Momani, Abdullah, Supyan Hussin, and Afendi Hamat. ",
+          subtitle: "“In the Light of Reading Strategies Comprehension improvement (Text Book, Online Material and Smartphone Application).”, ",
+          italic: "Arab World English Journal. ",
+          pp: "Special issue on CALL 1 (2014): 15-29.",
+          url: "#",
+        },
+        {
+          title: "Borni Lafi, ",
+          subtitle: "“The Southern Agrarians as New Critics: From Society to Textuality_ and Back! in Textes et Actes de Lectures, de Comprehension et d’Interpretation”, ",
+          italic: "published by University of Kairouan’s Doctoral School., ",
+          pp: "Nouha Editions. Sfax. Tunisia. I.S.B.N: 978-9938-12-654-9. pp. 4-17.",
+          url: "#",
+        },
+        {
+          title: "Borni Lafi, ",
+          subtitle: "Not a single thing to see and then again, there is the whole world to look at’: Nature’s Sacredness in Erskine Caldwell’s Fiction.” in الرمز الديني في النصوص 2014.  ",
+          italic: "published by University of Kairouan’s Doctoral School ",
+          pp: "Nouha Editions. 2014. Sfax. Tunisia. I.S.B.N: 978-9938-819-07-6. pp. 223-239, (2014).",
+          url: "#",
+        },
+        ]
+      },
+      {
+      year: "2013",
+      list: [
+        {
+          title: "Borni Lafi, ",
+          subtitle: "“Keeping the Nigger in his place’: Othering, Construction and Dissemination of Evil in Erskine Caldwell’s South.” in Le Mal: Valeur & Discours. Volume II. 2013“",
+          italic: "published by University of Kairouan’s Research Unit: Language-Discourses-Society",
+          pp: "Nouha Editions. Sfax. Tunisia. I.S.B.N: 978-9938-819-06-9. pp. 203-226.",
+          url: "#",
+        },
+        ]
+      },
+      {
+      year: "2012",
+      list: [
+        {
+          title: "Orabi, Hebatullah ",
+          subtitle: "The Discourse of Persuasion in Prophet Muhammad’s Da’wah Letters. Deutschland: LAP",
+          italic: "#",
+          pp: ", (2012)",
+          url: "#",
+        },
+        ]
+      },
+    ]
+
+export default English
