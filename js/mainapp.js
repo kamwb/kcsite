a8c784c123526964090c92b86c89621377ef8ebc
NasserAlanazi <53240515+NasserAlanazi@users.noreply.github.com>
2022-01-25 00:42:08 +0300

    Update mainapp.js

--- js/mainapp.js
+++ js/mainapp.js
@@ -4,6 +4,27 @@ var app = new Vue({
       latestNews: [
         {
           title:
+            "اللقاء الثاني لبرنامج الإدارة الرقمية المتكاملة",
+          image:
+            "https://www.uj.edu.sa/Files/856/Galleries/7270/56019_IDM.JPG",
+          url: "#",
+        },
+{
+          title:
+            "عوداً حميداً وفصل دراسي موفق",
+          image:
+            "https://www.uj.edu.sa/Files/856/Galleries/7278/55981_IMG_3696.JPG",
+          url: "#",
+        },
+{
+          title:
+            "اللقاء الأول لبرنامج الإدراة الرقمية المتكاملة",
+          image:
+            "https://www.uj.edu.sa/Files/856/Galleries/7270/55983_%D8%B4%D8%B1%D9%8A%D8%AD%D8%A91.JPG",
+          url: "#",
+        },
+{
+          title:
             "زيارة وكيل الجامعة للتطوير والتنمية المستدامة لفرع الكامل",
           image:
             "https://www.uj.edu.sa/Files/856/Galleries/7270/55932_%D8%B4%D8%B1%D9%8A%D8%AD%D8%A94.JPG",
@@ -158,35 +179,40 @@ var app = new Vue({
         },
       ],
       importantTips: [
-      {
-          image:
-            "https://www.uj.edu.sa/Files/856/Galleries/7272/55952_%D8%AC%D8%AF%D9%88%D9%84%20%D8%A8%D8%AF%D9%8A%D9%84.jpg",
-          text: "جدول اختبارات الفترة الثانية (البديلة ) لطلاب وطالبات السنة التحضيرية",
-          url: "https://t.co/PbhrAsNniS?amp=1",
+        {
+            image:
+                "https://www.uj.edu.sa/Files/856/Galleries/7272/55952_%D8%AC%D8%AF%D9%88%D9%84%20%D8%A8%D8%AF%D9%8A%D9%84.jpg",
+            text: "جدول اختبارات الفترة الثانية (البديلة ) لطلاب وطالبات السنة التحضيرية",
+            url: "https://t.co/PbhrAsNniS?amp=1",
         },{
-          image:
-            "https://www.uj.edu.sa/Images/856/%D9%81%D8%A7%D8%B7%D9%85%D8%A9%20%D8%A7%D9%84%D8%B3%D9%84%D9%85%D9%8A/%D8%A7%D9%84%D8%AA%D9%82%D9%88%D9%8A%D9%85%20%D8%A7%D9%84%D8%A7%D9%83%D8%A7%D8%AF%D9%8A%D9%85%D9%8A%20%D9%84%D8%A5%D8%AD%D8%AF%D8%A7%D8%AB%20%D8%A7%D9%84%D9%81%D8%B5%D9%84%20%D8%A7%D9%84%D8%AF%D8%B1%D8%A7%D8%B3%D9%8A%20%D8%A7%D9%84%D8%AB%D8%A7%D9%86%D9%8A.jpg",
-          text: "التقويم الأكاديمي لأحداث الفصل الدراسي الثاني للعام الدراسي 1442-1443هـ",
-          url: "https://csa.uj.edu.sa/Pages-Semister2.aspx",
+            image:
+                "https://www.uj.edu.sa/Images/856/%D9%81%D8%A7%D8%B7%D9%85%D8%A9%20%D8%A7%D9%84%D8%B3%D9%84%D9%85%D9%8A/%D8%A7%D9%84%D8%AA%D9%82%D9%88%D9%8A%D9%85%20%D8%A7%D9%84%D8%A7%D9%83%D8%A7%D8%AF%D9%8A%D9%85%D9%8A%20%D9%84%D8%A5%D8%AD%D8%AF%D8%A7%D8%AB%20%D8%A7%D9%84%D9%81%D8%B5%D9%84%20%D8%A7%D9%84%D8%AF%D8%B1%D8%A7%D8%B3%D9%8A%20%D8%A7%D9%84%D8%AB%D8%A7%D9%86%D9%8A.jpg",
+            text: "التقويم الأكاديمي لأحداث الفصل الدراسي الثاني للعام الدراسي 1442-1443هـ",
+            url: "https://csa.uj.edu.sa/Pages-Semister2.aspx",
         },
       ],
       latestAchievements: [
-      {
-          image: "https://csa.uj.edu.sa/Images/856/جائزة%20الابداع.jpg",
-          text: "كلية العلوم والآداب بالكامل تفوز بجائزة الإبداع بالمحافظة لعام 1442هـ إحدى جوائز الابداع بمحافظات منطقة مكة المكرمة.",
-          url: "https://csa.uj.edu.sa/Pages-%d8%ac%d8%a7%d8%a6%d8%b2%d8%a9-%d8%a7%d9%84%d8%a7%d8%a8%d8%af%d8%a7%d8%b9.aspx",
+        {
+            image: "https://www.uj.edu.sa/Files/856/Galleries/7271/56020_4f3b3266922e.jpg",
+            text: "الطالب. فراس سعد السلمي من قسم اللغة الانجليزية يحقق المركز الأول في السباق",
+            url: "https://csa.uj.edu.sa/Pages-saad-alsulami.aspx",
         },
         {
-          image:
-            "https://www.uj.edu.sa/Files/856/Galleries/7271/55950_%D8%AA%D9%87%D9%86%D8%A6%D8%A9%20%D8%A7%D9%84%D8%A8%D8%B1%D9%88%D9%81%D9%8A%D8%B3%D9%88%D8%B1%20%D8%B9%D8%A8%D8%AF%D8%A7%D9%84%D8%A8%D8%A7%D8%B3%D8%B7%20%D9%85%D8%B1%D9%8A%D9%88%D8%AF.JPG",
-          text: "تهنئة البروفيسور عبدالباسط بمناسبة إدراج اسمه ضمن افضل 2% من علماء العالم الأكثر استشهادًا أكاديمياً في مختلف التخصصات للمرة الثانية.",
-          url: "#",
+            image: "https://csa.uj.edu.sa/Images/856/جائزة%20الابداع.jpg",
+            text: "كلية العلوم والآداب بالكامل تفوز بجائزة الإبداع بالمحافظة لعام 1442هـ إحدى جوائز الابداع بمحافظات منطقة مكة المكرمة.",
+            url: "https://csa.uj.edu.sa/Pages-%d8%ac%d8%a7%d8%a6%d8%b2%d8%a9-%d8%a7%d9%84%d8%a7%d8%a8%d8%af%d8%a7%d8%b9.aspx",
         },
         {
-          image:
-            "https://www.uj.edu.sa/Files/856/Galleries/7271/55951_%D8%B4%D8%B1%D9%8A%D8%AD%D8%A92.JPG",
-          text: "تهنئة البروفيسور عبدالباسط بمناسبة فوزه بجائزة عبدالحميد شومان للباحثين العرب .",
-          url: "#",
+            image:
+                "https://www.uj.edu.sa/Files/856/Galleries/7271/55950_%D8%AA%D9%87%D9%86%D8%A6%D8%A9%20%D8%A7%D9%84%D8%A8%D8%B1%D9%88%D9%81%D9%8A%D8%B3%D9%88%D8%B1%20%D8%B9%D8%A8%D8%AF%D8%A7%D9%84%D8%A8%D8%A7%D8%B3%D8%B7%20%D9%85%D8%B1%D9%8A%D9%88%D8%AF.JPG",
+            text: "تهنئة البروفيسور عبدالباسط بمناسبة إدراج اسمه ضمن افضل 2% من علماء العالم الأكثر استشهادًا أكاديمياً في مختلف التخصصات للمرة الثانية.",
+            url: "#",
+        },
+        {
+            image:
+                "https://www.uj.edu.sa/Files/856/Galleries/7271/55951_%D8%B4%D8%B1%D9%8A%D8%AD%D8%A92.JPG",
+            text: "تهنئة البروفيسور عبدالباسط بمناسبة فوزه بجائزة عبدالحميد شومان للباحثين العرب .",
+            url: "#",
         },
       ],
     },
