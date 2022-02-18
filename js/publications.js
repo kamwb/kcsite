2f79e8000246270668e97ba4b841cf66ddfa60b7
NasserAlanazi <53240515+NasserAlanazi@users.noreply.github.com>
2022-02-19 00:54:06 +0300

    Update publications.js

--- js/publications.js
+++ js/publications.js
@@ -27,12 +27,8 @@ var app = new Vue({
                 url: "http://csa.uj.edu.sa/Files/856/Files/154773_%d9%85%d8%b4%d8%b1%d9%88%d8%b9%20%d8%a7%d9%84%d9%85%d8%aa%d8%ad%d9%81%20%d8%a7%d9%84%d8%b9%d9%84%d9%85%d9%8a%20%d8%a8%d9%81%d8%b1%d8%b9%20%d8%ac%d8%a7%d9%85%d8%b9%d8%a9%20%d8%ac%d8%af%d8%a9%20%d8%a8%d8%a7%d9%84%d9%83%d8%a7%d9%85%d9%84.pdf",
             },
             {
-                title: "مجلة 1",
-                url: "http://csa.uj.edu.sa/Files/856/Files/154773_%d9%85%d8%ac%d9%84%d8%a9%201.pdf",
-            },
-            {
-                title: "مجلة 2",
-                url: "http://csa.uj.edu.sa/Files/856/Files/154773_%d9%85%d8%ac%d9%84%d8%a9%202.pdf",
+                title: "مجلة الكلية",
+                url: "http://csa.uj.edu.sa/Files/856/Files/154773_mag.pdf",
             },
         ]
     },
