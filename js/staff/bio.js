ea8ade18effa9fc031c8bc1ca65cabd5c5d09b1d
NasserAlanazi <53240515+NasserAlanazi@users.noreply.github.com>
2022-03-08 00:04:11 +0300

    Create bio.js

---
+++ js/staff/bio.js
@@ -0,0 +1,44 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+      males: [
+        {
+            name: "عبدالعزيز فضل المولى",
+            email: "amalmomanh@uj.edu.sa",
+            img: "",
+            cv: "http://csa-kbio.uj.edu.sa/Files/856005/Files/154777_CV-Prof.%20Abdelaziz.pdf",
+            caption: "أستاذ مساعد",
+        },
+        {
+            name: "تركي عمر حسن عصر",
+            email: "tasar@uj.edu.sa",
+            img: "",
+            cv: "http://csa-kbio.uj.edu.sa/Files/856005/Files/154777_CV%20of%20Dr.%20Turky%20Omar%20Asar.pdf",
+            caption: "أستاذ مساعد",
+        },
+        {
+            name: "عبدالباسط مريود",
+            email: "aalnadif@uj.edu.sa",
+            img: "",
+            cv: "http://csa-kbio.uj.edu.sa/Files/856005/Files/154777_CV-Prof.%20Mariod.pdf",
+            caption: "أستاذ مساعد",
+        },
+        {
+            name: "عبدالرحيم حميدة",
+            email: "AAAHMED@uj.edu.sa",
+            img: "",
+            cv: "http://csa-kbio.uj.edu.sa/Files/856005/Files/154777_Dr.%20Hommeida%20CV.pdf",
+            caption: "أستاذ مساعد",
+        },
+      ],
+      females: [
+        {
+            name: "هدى السيد",
+            email: "hemohammed@uj.edu.sa",
+            img: "",
+            cv: "http://csa-kbio.uj.edu.sa/Files/856005/Files/154777_CV-Dr.%20Hoda.pdf",
+            caption: "أستاذ مساعد",
+        },
+      ]
+    },
+  });
