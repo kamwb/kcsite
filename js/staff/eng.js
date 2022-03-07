24bdb2607f369239549162af59a79030d939c801
NasserAlanazi <53240515+NasserAlanazi@users.noreply.github.com>
2022-03-07 11:19:35 +0300

    Update eng.js

--- js/staff/eng.js
+++ js/staff/eng.js
@@ -1,1 +1,93 @@
-
+var app = new Vue({
+        el: "#app",
+        data: {
+          males: [
+            {
+                name: "Abdullah Al Momani",
+                email: "amalmomanh@uj.edu.sa",
+                img: "",
+                cv: "http://csa-enlan.uj.edu.sa/Files/856004/Files/154776_Abdullah%20Momani.docx",
+                caption: "أستاذ مساعد",
+            },
+            {
+                name: "Borni Mahmoud Lafi",
+                email: "04220323@uj.edu.sa",
+                img: "",
+                cv: "http://csa-enlan.uj.edu.sa/Files/856004/Files/154776_Borni%20Lafi.docx",
+                caption: "أستاذ مساعد",
+            },
+            {
+                name: "Mohammed Irfan Jowher",
+                email: "jowhermij@yahoo.com",
+                img: "",
+                cv: "http://csa-enlan.uj.edu.sa/Files/856004/Files/154776_CV%20Mohammed%20Irfan%20Jowher.docx",
+                caption: "أستاذ مساعد",
+            },
+            {
+                name: "Ibrahim Kurdi",
+                email: "ikurdi@uj.edu.sa",
+                img: "",
+                cv: "http://csa-enlan.uj.edu.sa/Files/856004/Files/154776_Ibrahim%20Kurdi.docx",
+                caption: "أستاذ مساعد",
+            },
+          ],
+          females: [
+            {
+                name: "BALQEES ALSULAMI",
+                email: "bnalsulami@uj.edu.sa",
+                img: "",
+                cv: "http://csa-enlan.uj.edu.sa/Files/856004/Files/154776_Balgees%20Alsulami%20CV.docx",
+                caption: "محاضر",
+            },
+            {
+                name: "Hebatullah A. Orabi",
+                email: "haorabi@uj.edu.sa",
+                img: "",
+                cv: "http://csa-enlan.uj.edu.sa/Files/856004/Files/154776_Hebatullah%20A.%20Orabi%20CV.docx",
+                caption: "أستاذ مساعد",
+            },
+            {
+                name: "Madiha AL-Sulami ",
+                email: "mmaalsulami@uj.edu.sa",
+                img: "",
+                cv: "http://csa-enlan.uj.edu.sa/Files/856004/Files/154776_Madiha%20Alsulami,%20CV%20.docx",
+                caption: "محاضر",
+            },
+            {
+                name: "Manal Alzahrani",
+                email: "04100727@uj.edu.sa",
+                img: "",
+                cv: "http://csa-enlan.uj.edu.sa/Files/856004/Files/154776_Manal%20Alzahrani%20CV.docx",
+                caption: "أستاذ مساعد",
+            },
+            {
+                name: "Nada	Alshehri",
+                email: "04101243@uj.edu.sa",
+                img: "",
+                cv: "http://csa-enlan.uj.edu.sa/Files/856004/Files/154776_Nada%20%20Alshehri%20CV%20.pdf",
+                caption: "أستاذ مساعد",
+            },
+            {
+                name: "Siham Asa’ad",
+                email: "saassad@uj.edu.sa",
+                img: "",
+                cv: "http://csa-enlan.uj.edu.sa/Files/856004/Files/154776_Siham%20Asa'ad%20CV.docx",
+                caption: "محاضر",
+            },
+            {
+                name: "Sumaiyah A.salam Turkistani",
+                email: "saturkistani@uj.edu.sa",
+                img: "",
+                cv: "http://csa-enlan.uj.edu.sa/Files/856004/Files/154776_Sumaiyah%20Turkistani%20CV.docx",
+                caption: "أستاذ مساعد",
+            },
+            {
+                name: "ZULFA ALSULAMI",
+                email: "Znalsulami1@uj.edu.sa",
+                img: "",
+                cv: "http://csa-enlan.uj.edu.sa/Files/856004/Files/154776_Zulfa%20Alsulami%20CV.docx",
+                caption: "محاضر",
+            },
+          ]
+        },
+      });
