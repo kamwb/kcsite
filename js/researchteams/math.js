05dd9cac3d7616accc1b46bbb49a95a9f9a1123c
NasserAlanazi <53240515+NasserAlanazi@users.noreply.github.com>
2022-03-23 01:20:35 +0300

    Create math.js

---
+++ js/researchteams/math.js
@@ -0,0 +1,161 @@
+const mathm = [
+              {
+                name: 'حل المعادلات التفاضلية بالطرق العددية',
+                leader: 'أ.د. طارق الزاكي',
+                members: [
+                'د. مراد الشامخ',
+                'د. عواطف الحويطي',
+                'د. الطيب محمد',
+                'د. منى مجذوب',
+                'د. ألاء محمود',
+                'أ. محمد باجابر',
+                ],
+                work: [
+                {
+          title: "Eltaib M. Abd Elmohmoud &Tarig M. Elzaki,",
+          subtitle: "“Elzaki Transform of Derivative Expressed by Heaviside Function”, ",
+          italic: "World Applied Sciences Journal. ",
+          pp: "Vol. 32 (8): 1686-1689, (2014).DOI: 10.5829/idosi.wasj.2014.32.08.893",
+          url: " ",
+        },
+        {
+          title: "Alla M. Elsheikh & Tarig. M. Elzaki, ",
+          subtitle: "“Variation Iteration Method for Solving Porous Medium Equation”. ",
+          italic: "International Journal of Development Research.",
+          pp: "Vol. 5, Issue, 06, pp. 4677-4680, (2015).",
+          url: " ",
+        },
+         {
+          title: "Mohammed A. Bajaber &Tarig. M. Elzaki, ",
+          subtitle: "“New Theorem (Bajaber Theorem) to Find the Division Remainder of Polynomials”. ",
+          italic: "International journal of Multidisciplinary Research and information, IJMRI.",
+          pp: "Vol. 1, no. 3:159-163 , (2015).",
+          url: " ",
+        },
+                  {
+          title: "Alla M. Elsheikh and Tarig M. Elzaki, ",
+          subtitle: "“Modified Variational Iteration Method for Solving Fourth Order Parabolic PDEs With Variable Coefficients.”. ",
+          italic: "Global Journal of Pure and Applied Mathematics.",
+          pp: "Vol. 12, no. 2, pp. 1587-1592, (2016).",
+          url: " ",
+        },
+                 {
+          title: "Mourad Chamekh, Tarig. M. Elzaki, ",
+          subtitle: "“Explicit solution for some generalized fluids in laminar flow with slip boundary conditions.”. ",
+          italic: "Journal of Mathematics and computer science.",
+          pp: "Vol. 18, PP. 272–281, (2018).",
+          url: " ",
+        },
+                          {
+          title: "Tarig M. Elzaki, Mourad Chamekh, ",
+          subtitle: "“New transform iterative method for solving some Klein-Gordon equations Aisha Abdullah Alderremya.”. ",
+          italic: "Results in Physics.",
+          pp: "Vol. 10, PP. 655–659, (2018).",
+          url: " ",
+        },
+                          {
+          title: "Tarig M. Elzaki, Mourad Chamekh, ",
+          subtitle: "“Solving Nonlinear Fractional Differential Equations using a New Decomposition Method.”. ",
+          italic: "Universal Journal of Applied Mathematics & Computation.",
+          pp: "Vol. 6, PP. 27-35, (2018).",
+          url: " ",
+        },
+                                  {
+          title: "Aisha Abdullah Alderremy, Tarig M. Elzaki, and Mourad Chamekh, ",
+          subtitle: "“Modified Adomian Decomposition Method to Solve Generalized Emden–Fowler Systems for Singular IVP.”. ",
+          italic: "Mathematical Problems in Engineering.",
+          pp: "Vol. 2019, Article ID 6097095, 6 pages, (2019).",
+          url: "https://doi.org/10.1155/2019/6097095",
+        },
+                                  {
+          title: "Mourad Chamekh, Tarig M. Elzaki, and Nabiha Brik, ",
+          subtitle: "“Semi analytical solution for some proportional delay diferential equations.”. ",
+          italic: "SN Applied Sciences.",
+          pp: "Vol. 2019, (2019).https://doi.org/10.1007/s42452-018-0130-8.",
+          url: " ",
+        },
+                                  {
+          title: "Alla M. Elsheikh, and Tarig M. Elzaki, ",
+          subtitle: "“Modified Variation Iteration Method And Laplace – Elzaki Transform For Solve Hirota, Schrodinger And Complex MKDV Equations.”. ",
+          italic: "World Journal of Engineering Research and Technology WJERT.",
+          pp: "Vol. 6, Issue 6, PP. 128-136, (2020).",
+          url: " ",
+        },
+                                        {
+          title: "Eltaib M. Abd Elmohmoud, and Tarig M. Elzaki, ",
+          subtitle: "“New Method With Laplace Transform To Solve Nonlinear PDES.”. ",
+          italic: "World Journal of Engineering Research and Technology WJERT.",
+          pp: "Vol. 6, Issue 4, (2020).",
+          url: " ",
+        },
+                                        {
+          title: "Amjad E. Hamza, Mohamed Z. Mohamed, Eltaib M. Abd Elmohmoud, and M. Magzoub, ",
+          subtitle: "“Conformable Sumudu Transform of Space-Time Fractional Telegraph Equation.”. ",
+          italic: "Abstract and Applied Analysis.",
+          pp: "Vol. 2021, Article ID 6682994, 6 pages, (2021).",
+          url: "https://doi.org/10.1155/2021/6682994 ",
+        },
+                                        {
+          title: "Mohamed Z. Mohamed, Tarig M. Elzaki, Mohamed S. Algolam, Eltaib M. Abd Elmohmoud , and Amjad E. Hamza., ",
+          subtitle: "“New Modified Variational Iteration Laplace Transform Method Compares Laplace Adomian Decomposition Method for Solution Time-Partial Fractional Differential Equations.”. ",
+          italic: "Journal of Applied Mathematics.",
+          pp: "Vol. (2021) |Article ID 6662645.",
+          url: "https://doi.org/10.1155/2021/6662645",
+        },
+                                        {
+          title: "Mourad Chamekh, Mohamed Ali Latrach, Tarig M. Elzaki, ",
+          subtitle: "“Approximate analytical solutions for some obstacle problems.”. ",
+          italic: "Journal of King Saud University – Science.",
+          pp: "Vol. 33, Issue 2, (2021).",
+          url: "https://doi.org/10.1016/j.jksus.2020.101259 ",
+        },
+                                        {
+          title: "Mourad Chamekh, Mohamed Ali Latrach, Tarig M. Elzaki, ",
+          subtitle: "“Approximate analytical solutions for some obstacle problems.”. ",
+          italic: "Journal of King Saud University–Science.",
+          pp: "Volume 33 (2021) 101259.",
+          url: "https://www.researchgate.net/publication/347429561 ",
+        },
+                                        {
+          title: "Tarig M. Elzaki, Shams A. Ahmed, Mounirah Areshi, Mourad Chamekh, ",
+          subtitle: "“Fractional partial differential equations and novel double integral transform.”. ",
+          italic: "Journal of King Saud University – Science.",
+          pp: "(2022).",
+          url: "https://doi.org/10.1016/j.jksus.2022.101832",
+        },
+                ]
+              },
+              {
+                name: 'التحليل الاحصائي المتقدم',
+                leader: 'د. محمود امرير',
+                members: [
+                'د. ياسر عابد',
+                'أ. كامل العنزي ',
+                'أ. ايمان الحارثي',
+                ],
+                work: []
+              },
+              {
+                name: 'النمذجة العشوائية وتطبيقاتها',
+                leader: 'د. مجدي السلمي',
+                members: [
+                'د. مراد الشامخ',
+                'د. ياسر عابد',
+                'أ. كامل العنزي ',
+                'أ. ايمان الحارثي',
+                'أ. نورة اللقماني',
+                ],
+                work: []
+              },
+              {
+                name: 'الهندسة الريمانية',
+                leader: 'موسى الجوارنة',
+                members: [
+                'أ. عبد الرحمان الثبيتي',
+                'أ. أشواق الكاسب',
+                ],
+                work: []
+              },
+            ]
+
+export default mathm
