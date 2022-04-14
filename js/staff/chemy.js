c8a0aebac6cccc8a428566a74c975c61da035835
NasserAlanazi <53240515+NasserAlanazi@users.noreply.github.com>
2022-04-15 02:25:31 +0300

    Create chemy.js

---
+++ js/staff/chemy.js
@@ -0,0 +1,37 @@
+var app = new Vue({
+        el: "#app",
+        data: {
+          males: [
+            {
+                name: "محمد أمين بن علي",
+                email: "maali@uj.edu.sa",
+                img: "",
+                cv: "http://csa-kchem.uj.edu.sa/Files/856008/Files/154775_CV-Dr%20Amine.pdf",
+                caption: "أستاذ مساعد",
+            },
+            {
+                name: "فتح الرحمن الحسن",
+                email: "Fhalhassan@uj.edu.sa",
+                img: "",
+                cv: "http://csa-kchem.uj.edu.sa/Files/856008/Files/154775_Fatah-CV.pdf",
+                caption: "أستاذ مساعد",
+            },
+            {
+                name: "أحمد سليمان حسين",
+                email: "aseisa@uj.edu.sa",
+                img: "",
+                cv: "http://csa-kchem.uj.edu.sa/Files/856008/Files/154775_DR%20AHMED%20SULIMAN%20HUSSEIN.pdf",
+                caption: "أستاذ مساعد",
+            },
+            {
+                name: "وسام نادر",
+                email: "wslamah@uj.edu.sa",
+                img: "",
+                cv: "http://csa-kchem.uj.edu.sa/Files/856008/Files/154775_Wessam-Nader.pdf",
+                caption: "أستاذ مساعد",
+            },
+          ],
+          females: [
+          ]
+        },
+      });
