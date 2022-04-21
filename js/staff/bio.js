56292f23e16567026f1e22624986830f22ed4478
yasserayed <95164108+yasserayed@users.noreply.github.com>
2022-04-21 13:50:01 +0300

    Update bio.js

--- js/staff/bio.js
+++ js/staff/bio.js
@@ -1,28 +1,29 @@
 var app = new Vue({
     el: "#app",
     data: {
-      males: [
-        {
+     males: [
+       {
+            name: "عبدالباسط مريود",
+            email: "aalnadif@uj.edu.sa",
+            img: "https://www.uj.edu.sa/Files/856006/Galleries/7292/56216_prof.Mariod5.jpg",
+            cv: "http://csa-kbio.uj.edu.sa/Files/856005/Files/154777_CV-Prof.%20Mariod.pdf",
+            caption: "أستاذ دكتور",
+        },
+  {
             name: "عبدالعزيز فضل المولى",
-            email: "amalmomanh@uj.edu.sa",
-            img: "",
+            email: "aasalem@uj.edu.sa",
+            img: "https://www.uj.edu.sa/Files/856006/Galleries/7292/56217_Abdelaziz.jpeg",
             cv: "http://csa-kbio.uj.edu.sa/Files/856005/Files/154777_CV-Prof.%20Abdelaziz.pdf",
-            caption: "أستاذ مساعد",
+            caption: "أستاذ دكتور",
         },
         {
             name: "تركي عمر حسن عصر",
             email: "tasar@uj.edu.sa",
-            img: "",
+            img: "https://www.uj.edu.sa/Files/856006/Galleries/7292/56219_Taser.jpeg",
             cv: "http://csa-kbio.uj.edu.sa/Files/856005/Files/154777_CV%20of%20Dr.%20Turky%20Omar%20Asar.pdf",
             caption: "أستاذ مساعد",
         },
-        {
-            name: "عبدالباسط مريود",
-            email: "aalnadif@uj.edu.sa",
-            img: "",
-            cv: "http://csa-kbio.uj.edu.sa/Files/856005/Files/154777_CV-Prof.%20Mariod.pdf",
-            caption: "أستاذ مساعد",
-        },
+       
         {
             name: "عبدالرحيم حميدة",
             email: "AAAHMED@uj.edu.sa",
