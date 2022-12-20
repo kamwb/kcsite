c8d85c80657f4c873af22c0ef5f258052afdf36b
yasserayed <95164108+yasserayed@users.noreply.github.com>
2022-12-20 21:27:58 +0300

    Update chemy.js

--- js/staff/chemy.js
+++ js/staff/chemy.js
@@ -3,25 +3,13 @@ var app = new Vue({
         data: {
           males: [
             {
-                name: "أحمد سليمان حسين",
-                email: "aseisa@uj.edu.sa",
-                img: "",
-                cv: "http://csa-kchem.uj.edu.sa/Files/856008/Files/154775_DR%20AHMED%20SULIMAN%20HUSSEIN.pdf",
-                caption: "أستاذ مساعد",
-            },  {
                 name: "وسام نادر",
                 email: "wslamah@uj.edu.sa",
                 img: "https://www.uj.edu.sa/Files/856006/Galleries/7292/56223_wesam.jpg",
                 cv: "http://csa-kchem.uj.edu.sa/Files/856008/Files/154775_Wessam-Nader.pdf",
                 caption: "أستاذ مساعد",
             },
-                  {
-                name: "محمد أمين بن علي",
-                email: "maali@uj.edu.sa",
-                img: "",
-                cv: "http://csa-kchem.uj.edu.sa/Files/856008/Files/154775_CV-Dr%20Amine.pdf",
-                caption: "أستاذ مساعد",
-            },
+               
             {
                 name: "فتح الرحمن الحسن",
                 email: "Fhalhassan@uj.edu.sa",
